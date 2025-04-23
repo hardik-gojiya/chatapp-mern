2b1535935eb5d39b68614a07ab3d7763418cd7ee
Hardikgojiya <hardikgojiya143@gmail.com>
2025-04-23 17:53:23 +0530

    photo download feature

--- frontend/src/components/ShowImage.jsx
+++ frontend/src/components/ShowImage.jsx
@@ -1,6 +1,28 @@
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useToast } from "../context/ToastContext";
 
 function ShowImage({ showImage, setShowImage }) {
+  const { showError } = useToast();
+  const handleDownloadImage = async () => {
+    try {
+      const response = await fetch(showImage);
+      const blob = await response.blob();
+
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "chat-image.png";
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+      window.URL.revokeObjectURL(url);
+    } catch (err) {
+      showError("Failed to download image", err);
+    }
+  };
+
   return (
     <div>
       {showImage && (
@@ -10,7 +32,7 @@ function ShowImage({ showImage, setShowImage }) {
         >
           <div
             className="relative max-w-full max-h-full p-2 sm:p-4 md:p-6"
-            onClick={(e) => e.stopPropagation()} // prevent closing when clicking image
+            onClick={(e) => e.stopPropagation()}
           >
             <img
               src={showImage}
@@ -23,6 +45,12 @@ function ShowImage({ showImage, setShowImage }) {
               className="fixed top-2 right-2 sm:top-4 sm:right-4 bg-red-500 text-white rounded-full h-8 w-8 flex items-center justify-center shadow-md hover:bg-red-600 transition"
             >
               ✕
+            </button>
+            <button
+              onClick={handleDownloadImage}
+              className="fixed top-10 right-2 mt-4 sm:top-4 sm:right-4 bg-gray-500  text-white rounded-full h-8 w-8 flex items-center justify-center shadow-md hover:bg-red-600 transition"
+            >
+              <FontAwesomeIcon icon={faArrowDown} />
             </button>
           </div>
         </div>
